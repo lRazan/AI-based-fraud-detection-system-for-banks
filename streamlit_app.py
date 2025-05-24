8d1e399e5778c63e582667ae572135b03ea2eac3
Razan Bajaman <440022262@sm.imamu.edu.sa>
2025-05-25 01:38:10 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,20 @@
 import streamlit as st 
-    #</style>
-    # st.markdown(\"\"\"
-    #     <div class="background-image"></div>
-    #     <style>
-    #         .background-image {
-    #             background: url('https://images.unsplash.com/photo-1591696205602-2f950c417cb9?ixlib=rb-4.0.3&auto=format&fit=crop&w=2100&q=80') center/cover no-repeat;
-    #             position: fixed;
-    #             top: 0;
-    #             left: 0;
-    #             width: 100vw;
-    #             height: 100vh;
-    #             z-index: -1;
-    #             pointer-events: none;
-    #         }
-    #     </style>
-    # \"\"\", unsafe_allow_html=True)
+import streamlit as st 
+st.markdown("""
+    <div class="background-image"></div>
+    <style>
+        .background-image {
+            background: url('https://images.unsplash.com/photo-1591696205602-2f950c417cb9?ixlib=rb-4.0.3&auto=format&fit=crop&w=2100&q=80') center/cover no-repeat;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100vw;
+            height: 100vh;
+            z-index: -1;
+            pointer-events: none;
+        }
+    </style>
+""", unsafe_allow_html=True)
 import pandas as pd
 import pymysql
 import matplotlib.pyplot as plt 
